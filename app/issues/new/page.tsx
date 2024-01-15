71c4160d30da78b13dc102a0b8493375b84e203c
Shihan Nuwanjith <shihannuwanjith@gmail.com>
2024-01-15 20:31:06 +0530

    customize radix ui theme

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -5,14 +5,14 @@ import {TextField, TextArea, Button} from '@radix-ui/themes'
 
 const NewIssuePage = () => {
   return (
-    <div className = 'max-w-xl space-y-3'>
-        <TextField.Root>
-            <TextField.Input placeholder='Title'/>
-        </TextField.Root>
-        <TextArea placeholder='Description'/>
-        <Button>Submit New Issue</Button>
+    <div className="max-w-xl space-y-3">
+      <TextField.Root>
+        <TextField.Input placeholder="Title" />
+      </TextField.Root>
+      <TextArea placeholder="Description" />
+      <Button>Submit New Issue</Button>
     </div>
-  )
+  );
 }
 
 export default NewIssuePage
